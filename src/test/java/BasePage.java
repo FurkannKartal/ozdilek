f64b4eaf4227f59afcc6f84e9a4e4968cab6aadb
furkan kartal <fkartal@gmail.com>
2022-06-22 11:32:58 +0300

    Logger commit

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -32,33 +32,33 @@ public class BasePage extends BaseTest {
     @Step("id<id> li elementli kontrol et")
     public void checkText1(String id) {
         String chectText1 = appiumDriver.findElement(By.id(id)).getText();
-        Logger.info("Alisveris sayfasi basariyla acildi.");
+        logger.info("Alisveris sayfasi basariyla acildi.");
     }
 
     @Step("id<id> kategoriye tıkla")
     public void clickBykategori(String id) {
         appiumDriver.findElement(By.id(id)).click();
-        Logger.info("Kategoriler sayfasina tiklandi");
+        logger.info("Kategoriler sayfasina tiklandi");
     }
 
     @Step("xpath<xpath> kadın kategorisine tıkla")
     public void clickBykadın(String xpath) {
         appiumDriver.findElement(By.xpath(xpath)).click();
-        Logger.info("Kadin kategorisine tiklandi.");
+        logger.info("Kadin kategorisine tiklandi.");
     }
 
 
     @Step("xpath<xpath> pantolon kategorisine tıkla")
     public void clickBypantolon(String xpath) {
         appiumDriver.findElement(By.xpath(xpath)).click();
-        Logger.info("Pantolon kategorisine tiklandi.");
+        logger.info("Pantolon kategorisine tiklandi.");
     }
 
     @Step("scroll et")
     public void scrollEt() {
         TouchAction ts = new TouchAction(appiumDriver);
         ts.press(PointOption.point(538, 1538)).moveTo(PointOption.point(538, 420)).release().perform();
-        Logger.info("Scroll yapildi.");
+        logger.info("Scroll yapildi.");
     }
 
     @Step("Rastgele bir ürün seçme")
@@ -69,7 +69,7 @@ public class BasePage extends BaseTest {
         MobileElement element = randomUrun.get(random.nextInt(randomUrun.size()));
         System.out.println("element" + element);
         element.click();
-        Logger.info("Rastele bir urun secildi.");
+        logger.info("Rastele bir urun secildi.");
     }
 
 
@@ -77,45 +77,45 @@ public class BasePage extends BaseTest {
     public void urunDetay(String id) {
         String urunDetay = appiumDriver.findElement(By.id(id)).getText();
         Assertions.assertEquals("Beden:", urunDetay);
-        Logger.info("Urun detay kontrolu yapildi.");
+        logger.info("Urun detay kontrolu yapildi.");
     }
 
     @Step("xpath <xpath> favoriye ekle")
     public void favorilereEkle(String xpath){
         appiumDriver.findElement(By.xpath(xpath)).click();
-        Logger.info("Urun favorilere eklendi.");
+        logger.info("Urun favorilere eklendi.");
     }
 
     @Step("id<id> giris kontrolü")
     public void girisKontrol(String id) {
         String girisKontrol = appiumDriver.findElement(By.id(id)).getText();
         Assertions.assertEquals("Beni Hatırla", girisKontrol);
-        Logger.info("Giris kontrolu yapildi.");
+        logger.info("Giris kontrolu yapildi.");
     }
 
 
     @Step("<id> id'li elemente <mail> mail adresi girme")
     public void kullaniciAdi(String mailId,String mailText){
         appiumDriver.findElement(By.id(mailId)).sendKeys(mailText);
-        Logger.info("Mail adresi girildi");
+        logger.info("Mail adresi girildi");
     }
 
     @Step("<id> 'li elemente <sifre> sifre girme")
     public void kullaniciSifre(String sifreId, String sifreText){
         appiumDriver.findElement(By.id(sifreId)).sendKeys(sifreText);
-        Logger.info("Sifre girildi");
+        logger.info("Sifre girildi");
     }
 
     @Step("id<id> geri tusuna tıkla")
     public void clickBygeri(String id) {
         appiumDriver.findElement(By.id(id)).click();
-        Logger.info("Geri tusuna basildi.");
+        logger.info("Geri tusuna basildi.");
     }
 
     @Step("<id> sepete ekleme yap")
     public  void AddToCart(String sepetId){
         appiumDriver.findElement(By.id(sepetId)).click();
-        Logger.info("Sepete ekle tusuna basildi");
+        logger.info("Sepete ekle tusuna basildi");
     }
 
 }
